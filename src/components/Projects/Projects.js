af4afec265b5eafb976113a7dd9b9e97f3502d96
CodeNova-Kumar <KUMARVERMA2002@GMAIL.COM>
2024-05-03 22:27:20 +0530

    ok

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import Resume from "../../Assets/Projects/Resume.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import landing from "../../Assets/Projects/landing.png";
+import Loginp from "../../Assets/Projects/Loginp.png";
+import Meal from "../../Assets/Projects/Meal.png";
+import Lfashion from "../../Assets/Projects/Lfashion.png";
 
 function Projects() {
   return (
@@ -23,57 +23,57 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Loginp}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              title="Animated Login Page"
+              description="The Front-End Animated Login Page project is a dynamic and visually engaging login page designed to enhance the user experience on websites and web applications.It's fully responsive for all the platforms using React."
+              ghLink="https://github.com/CodeNova-Kumar/Animated-Login-Page"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Lfashion}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Lucknow Fashion"
+              description="My personal site Lucknow Fashion page build with HTML, CSS and JavaScript, Responsive for all devices"
+              ghLink="https://github.com/CodeNova-Kumar/Lucknow-Fashion-Site"
+              demoLink="https://codenova-kumar.github.io/Lucknow-Fashion-Site/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={landing}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              title="Landing Page"
+              description="This gorgeous responsive website is a static landing page built with React, react-router, and react hooks. All the components are reusable and can be used for any dynamic content. It's fully responsive for all the platforms and hosted using Netlify"
+              ghLink="https://github.com/CodeNova-Kumar/Landing-Page"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Resume}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Resume"
+              description="What is known as resume? A resume is a document summarizing your education, work experience, and skills. The length of a resume may depend upon how many years of work experience you have."
+              ghLink="https://github.com/CodeNova-Kumar/Resume"
+              demoLink="https://codenova-kumar.github.io/Resume/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={Meal}
               isBlog={false}
-              title="Ai For Social Good"
+              title="Meal Finder"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
+              ghLink="https://github.com/CodeNova-Kumar/Meal_Finder?tab=readme-ov-file"
+              demoLink="https://codenova-kumar.github.io/Meal_Finder/"
+              />
           </Col>
 
           <Col md={4} className="project-card">
